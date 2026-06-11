61c2e16bf34b6fec4ce03b2e522d3e5c0b135cf4
dev-sajanverma <dev.sajanverma@gmail.com>
2026-06-11 12:31:57 +0530

    created home-page UI

---
+++ resources/views/home.blade.php
@@ -0,0 +1,318 @@
+@extends('layout.main')
+@section('content')
+
+<!-- slider start here  -->
+<div id="carouselExampleIndicators" class="carousel slide">
+    <div class="carousel-indicators">
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="3" aria-label="Slide 4"></button>
+    </div>
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="{{ asset('assets/images/slider1.png') }}" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="{{ asset('assets/images/slider2.png') }}" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="{{ asset('assets/images/slider3.png') }}" class="d-block w-100" alt="...">
+        </div>
+        <div class="carousel-item">
+            <img src="{{ asset('assets/images/slider4.png') }}" class="d-block w-100" alt="...">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
+<!-- slider end here    -->
+
+
+<!-- product section start here   -->
+<section class="py-3 pt-lg-5 ">
+    <div class="container">
+        <div class="d-flex">
+            <div class="p-2 flex-grow-1">
+                <h2 class="mb-3">Top Deals</h2>
+            </div>
+            <div class="p-2">
+                <a href="#" class="text-decoration-none text-sm text-light btn btn-success btn-sm">View All</a>
+            </div>
+
+        </div>
+        <div class="row theme-products">
+            <div class="col-lg-3 col-md-6 col-12 mb-3">
+                <div class="card overflow-hidden ">
+                    <a href="#" class="text-decoration-none">
+                        <img src="{{ asset('assets/images/products/1.jpg') }}" class="card-img-top" alt="...">
+                    </a>
+                    <div class="card-body">
+                        <a href="#" class="text-decoration-none text-dark">
+                            <h6 class="card-title text-center">Cumpus Shoes</h6>
+                        </a>
+                        <h4 class="card-title text-center">$99.99</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-12 mb-3">
+                <div class="card overflow-hidden ">
+                    <a href="#" class="text-decoration-none">
+                        <img src="{{ asset('assets/images/products/2.jpg') }}" class="card-img-top" alt="...">
+                    </a>
+                    <div class="card-body">
+                        <a href="#" class="text-decoration-none text-dark">
+                            <h6 class="card-title text-center">Apple Watch</h6>
+                        </a>
+                        <h4 class="card-title text-center">$99.99</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-12 mb-3">
+                <div class="card overflow-hidden ">
+                    <a href="#" class="text-decoration-none">
+                        <img src="{{ asset('assets/images/products/3.jpg') }}" class="card-img-top" alt="...">
+                    </a>
+                    <div class="card-body overflow-hidden ">
+                        <a href="#" class="text-decoration-none text-dark">
+                            <h6 class="card-title text-center">Nike Cap</h6>
+                        </a>
+                        <h4 class="card-title text-center">$99.99</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-12 ">
+                <div class="card overflow-hidden ">
+                    <a href="#" class="text-decoration-none">
+                        <img src="{{ asset('assets/images/products/4.jpg') }}" class="card-img-top" alt="...">
+                    </a>
+                    <div class="card-body">
+                        <a href="#" class="text-decoration-none text-dark">
+                            <h6 class="card-title text-center">Wooden Chair</h6>
+                        </a>
+                        <h4 class="card-title text-center">$99.99</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- product section end here    -->
+
+
+<!-- Best of Electronics section start here     -->
+<section class="py-3  ">
+    <div class="container">
+        <div class="d-flex">
+            <div class="p-2 flex-grow-1">
+                <h2 class="mb-3">Best of Electronics</h2>
+            </div>
+            <div class="p-2">
+                <a href="#" class="text-decoration-none text-sm text-light btn btn-success btn-sm">View All</a>
+            </div>
+
+        </div>
+        <div class="row theme-products">
+            <div class="col-lg-3 col-md-6 col-12 mb-3">
+                <div class="card overflow-hidden ">
+                    <a href="#" class="text-decoration-none">
+                        <img src="{{ asset('assets/images/products/2.jpg') }}" class="card-img-top" alt="...">
+                    </a>
+                    <div class="card-body">
+                        <a href="#" class="text-decoration-none text-dark">
+                            <h6 class="card-title text-center">Cumpus Shoes</h6>
+                        </a>
+                        <h4 class="card-title text-center">$99.99</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-12 mb-3">
+                <div class="card overflow-hidden ">
+                    <a href="#" class="text-decoration-none">
+                        <img src="{{ asset('assets/images/products/5.jpg') }}" class="card-img-top" alt="...">
+                    </a>
+                    <div class="card-body">
+                        <a href="#" class="text-decoration-none text-dark">
+                            <h6 class="card-title text-center">Apple Watch</h6>
+                        </a>
+                        <h4 class="card-title text-center">$99.99</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-12 mb-7">
+                <div class="card overflow-hidden ">
+                    <a href="#" class="text-decoration-none">
+                        <img src="{{ asset('assets/images/products/7.jpg') }}" class="card-img-top" alt="...">
+                    </a>
+                    <div class="card-body overflow-hidden ">
+                        <a href="#" class="text-decoration-none text-dark">
+                            <h6 class="card-title text-center">Nike Cap</h6>
+                        </a>
+                        <h4 class="card-title text-center">$99.99</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-12 ">
+                <div class="card overflow-hidden ">
+                    <a href="#" class="text-decoration-none">
+                        <img src="{{ asset('assets/images/products/8.jpg') }}" class="card-img-top" alt="...">
+                    </a>
+                    <div class="card-body">
+                        <a href="#" class="text-decoration-none text-dark">
+                            <h6 class="card-title text-center">Wooden Chair</h6>
+                        </a>
+                        <h4 class="card-title text-center">$99.99</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Best of Electronics section end here      -->
+
+<!-- Popular categories section start here      -->
+<section class="py-3  ">
+    <div class="container">
+        <div class="d-flex">
+            <div class="p-2 flex-grow-1">
+                <h2 class="mb-3">Popular Categories</h2>
+            </div>
+            <div class="p-2">
+                <a href="#" class="text-decoration-none text-sm text-light btn btn-success btn-sm">View All</a>
+            </div>
+
+        </div>
+        <div class="row theme-products">
+            <div class="col-lg-3 col-md-6 col-12 mb-3">
+                <div class="card overflow-hidden ">
+                    <a href="#" class="text-decoration-none">
+                        <img src="{{ asset('assets/images/products/9.jpg') }}" class="card-img-top" alt="...">
+                    </a>
+                    <div class="card-body">
+                        <a href="#" class="text-decoration-none text-dark">
+                            <h6 class="card-title text-center">Hand Bag</h6>
+                        </a>
+                        <h4 class="card-title text-center">$99.99</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-12 mb-3">
+                <div class="card overflow-hidden ">
+                    <a href="#" class="text-decoration-none">
+                        <img src="{{ asset('assets/images/products/10.jpg') }}" class="card-img-top" alt="...">
+                    </a>
+                    <div class="card-body">
+                        <a href="#" class="text-decoration-none text-dark">
+                            <h6 class="card-title text-center">Laptop bag</h6>
+                        </a>
+                        <h4 class="card-title text-center">$99.99</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-12 mb-7">
+                <div class="card overflow-hidden ">
+                    <a href="#" class="text-decoration-none">
+                        <img src="{{ asset('assets/images/products/7.jpg') }}" class="card-img-top" alt="...">
+                    </a>
+                    <div class="card-body overflow-hidden ">
+                        <a href="#" class="text-decoration-none text-dark">
+                            <h6 class="card-title text-center">Nike Cap</h6>
+                        </a>
+                        <h4 class="card-title text-center">$99.99</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-12 ">
+                <div class="card overflow-hidden ">
+                    <a href="#" class="text-decoration-none">
+                        <img src="{{ asset('assets/images/products/8.jpg') }}" class="card-img-top" alt="...">
+                    </a>
+                    <div class="card-body">
+                        <a href="#" class="text-decoration-none text-dark">
+                            <h6 class="card-title text-center">Wooden Chair</h6>
+                        </a>
+                        <h4 class="card-title text-center">$99.99</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Popular categories section end here     -->
+
+<!-- Recently-viewed section start here    -->
+<section class="py-3 pb-lg-5 ">
+    <div class="container">
+        <div class="d-flex">
+            <div class="p-2 flex-grow-1">
+                <h2 class="mb-3">Recently Viewed</h2>
+            </div>
+            <div class="p-2">
+                <a href="#" class="text-decoration-none text-sm text-light btn btn-primary btn-sm">View All</a>
+            </div>
+
+        </div>
+        <div class="row theme-products">
+            <div class="col-lg-3 col-md-6 col-12 mb-3">
+                <div class="card overflow-hidden ">
+                    <a href="#" class="text-decoration-none">
+                        <img src="{{ asset('assets/images/products/5.jpg') }}" class="card-img-top" alt="...">
+                    </a>
+                    <div class="card-body">
+                        <a href="#" class="text-decoration-none text-dark">
+                            <h6 class="card-title text-center">Camera</h6>
+                        </a>
+                        <h4 class="card-title text-center">$99.99</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-12 mb-3">
+                <div class="card overflow-hidden ">
+                    <a href="#" class="text-decoration-none">
+                        <img src="{{ asset('assets/images/products/6.jpg') }}" class="card-img-top" alt="...">
+                    </a>
+                    <div class="card-body">
+                        <a href="#" class="text-decoration-none text-dark">
+                            <h6 class="card-title text-center">Leather Shoes</h6>
+                        </a>
+                        <h4 class="card-title text-center">$99.99</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-12 mb-3">
+                <div class="card overflow-hidden ">
+                    <a href="#" class="text-decoration-none">
+                        <img src="{{ asset('assets/images/products/7.jpg') }}" class="card-img-top" alt="...">
+                    </a>
+                    <div class="card-body overflow-hidden ">
+                        <a href="#" class="text-decoration-none text-dark">
+                            <h6 class="card-title text-center">LED</h6>
+                        </a>
+                        <h4 class="card-title text-center">$99.99</h4>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 col-12 ">
+                <div class="card overflow-hidden ">
+                    <a href="#" class="text-decoration-none">
+                        <img src="{{ asset('assets/images/products/8.jpg') }}" class="card-img-top" alt="...">
+                    </a>
+                    <div class="card-body">
+                        <a href="#" class="text-decoration-none text-dark">
+                            <h6 class="card-title text-center">Washing Machine</h6>
+                        </a>
+                        <h4 class="card-title text-center">$99.99</h4>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<!-- Recently-viewed section end here  -->
+@endsection
---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,77 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Home Page</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css" integrity="sha512-2SwdPD6INVrV/lHTZbO2nodKhrnDdJK9/kg2XD1r9uGqPo1cUbujc+IYdlYdEErWNu69gVcYgdxlmVmzTWnetw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="{{ asset('assets/css/style.css') }}">
+    <!-- Google Fonts se Lato Font ka link -->
+    <link rel="preconnect" href="https://googleapis.com">
+    <link rel="preconnect" href="https://gstatic.com" crossorigin>
+    <link href="https://googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;        1,700;1,900&display=swap" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+    <!-- // navbar start here -->
+    <nav class="navbar navbar-expand-lg theme-navbar">
+        <div class="container">
+            <a class="navbar-brand text-light" href="{{ url('/') }}">E-commerce</a>
+            <button class="navbar-toggler bg-light" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <div class="mx-auto my-2 my-lg-0 w-100 w-lg-auto" style="max-width: 400px;">
+                    <form class="d-flex" role="search">
+                        <div class="input-group w-100">
+                            <input class="form-control form-control-sm border-0 shadow-none" type="search" placeholder="Search for products" aria-label="Search" />
+                            <button class="btn btn-light btn-sm" type="submit"><i class="fa-solid fa-magnifying-glass"></i></button>
+                        </div>
+                    </form>
+                </div>
+                <div class="d-flex flex-column flex-lg-row align-items-start align-items-lg-center gap-2 gap-lg-0">
+                    <a href="#" class="text-decoration-none text-light me-lg-3 my-2 my-lg-0 text-sm">Become a Seller</a>
+                    <a href="#" class="btn theme-green-btn text-light btn-sm me-lg-2 w-100 w-lg-30"><i class="fas fa-shopping-cart"></i> Cart</a>
+                    <a href="#" class="btn theme-orange-btn text-light btn-sm w-100 w-lg-30"><i class="fa-solid fa-user"></i> Login</a>
+                </div>
+
+            </div>
+        </div>
+    </nav>
+    <!-- // navbar end here -->
+
+    <!-- category start here   -->
+    <nav class="navbar p-0">
+        <div class="container-fluid theme-navbar-light">
+            <div class="w-100">
+
+                <ul class="nav justify-content-center text-nowrap pb-2">
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="#">Phone</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="#">Fashion</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="#">Electronics</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="#">Furniture</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="#">Grocery</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-dark" href="#">Appliances</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <!-- category end here    -->
